e67e5ad9896c282aab5133151d3b0d76dde9968b
barisgirayakman <girayakman@gmail.com>
2023-11-30 01:44:59 +0300

    Negative to binary conversion added

--- converter.py
+++ converter.py
@@ -28,9 +28,9 @@ def convert_ADD(instruction: str) -> str:
             result += bin(int(instruction[2]))[2:].zfill(6)
         else:
             # negative value
-            result += (6 - len(bin(int(instruction[2]))[2:])) * "1" + bin(
+            result += (6 - len(bin(int(instruction[2]))[3:])) * "1" + bin(
                 int(instruction[2])
-            )[2:]
+            )[3:]
     else:
         instruction = instruction[1].split(",")
         result += "0000"
@@ -42,4 +42,53 @@ def convert_ADD(instruction: str) -> str:
 
 
 l = read_file("input.txt")
-convert_ADD(l[0])
+print(convert_ADD(l[1]))
+
+
+
+print(bin(-12))
+
+
+# It returns the length of the bit representation of the number.
+def find_max(num):
+    val = 1
+    num=-num
+    length=0
+    while val<num:
+        val*=2
+        length+=1
+    return length+1
+
+
+def convert_int(negative_string):
+    result=0
+    length = len(negative_string)
+    for i in range(len(negative_string)):
+        if i==0:
+            result=-pow(2, length-i-1)
+        elif negative_string[i]=="1":
+            result+=pow(2, length-i-1)
+    return result
+
+# Negative decimal to binary converter
+# For each iteration, current state of the binary number is calculated.
+# Corresponding bits are swapped through 0 and 1 for each iteration.
+def generate_negative(num):
+    length=find_max(num)
+    bit_representation="1"+"0"*(length-1)
+    bit_representation=list(bit_representation)
+    i=1
+    result=convert_int(bit_representation)
+    for i in range(len(bit_representation)):
+        if convert_int(bit_representation)<num:
+            bit_representation[i]="1"
+        elif convert_int(bit_representation)>num:
+            bit_representation[i-1]="0"
+            bit_representation[i]="1"
+    return "".join(bit_representation)
+
+
+
+print(generate_negative(-12))
+
+    
